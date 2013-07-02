bb4a325f26cbd858c85a2b34857c7a38ca306b88
jcm872000 <jcm872000@gmail.com>
2013-07-02 13:10:55 +0800

    dev:服务器对于不同路径的页面响应

--- src/main/webservice.py
+++ src/main/webservice.py
@@ -7,18 +7,17 @@ CMD_dom = xml.dom.minidom.parse(CMD_Str_path)
 command_lists = CMD_dom.getElementsByTagName("command")
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write("Hello world")
+    def get(self):
+        self.write("hello world")
 
 class MetaHandler(tornado.web.RequestHandler):
-	def get(self):
-		for command_node in command_lists:
-			print command_node.childNodes[0].data
-			self.write(command_node.childNodes[0].data+"<br/>")
+    def get(self):
+        for command_node in command_lists:
+            print command_node.childNodes[0].data
+            self.write(command_node.childNodes[0].data+"<br/>")
 
-application = tornado.web.Application([(r"/main/", MainHandler),])
-application = tornado.web.Application([(r"/run/", MetaHandler),])
+application = tornado.web.Application([(r"/", MainHandler),(r"/run/",MetaHandler)])
 
 if __name__ == "__main__":
-	application.listen(8888)
-	tornado.ioloop.IOLoop.instance().start()
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
